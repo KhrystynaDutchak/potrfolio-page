d1a08d4f937816df33715c6e3bbbf33626233dc2
DutchakKristina <dutchakk7@gmail.com>
2024-03-16 17:50:44 -0600

    vite

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    root: 'src',
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: {
+          main: './src/index.html',
+        },
+        output: {
+          entryFileNames: 'commonHelpers.js',
+        },
+      },
+      outDir: '../dist',
+    },
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html']),
+    ],
+  };
+});
